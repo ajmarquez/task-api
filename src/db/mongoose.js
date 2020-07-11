cfcd1eba50aa3a4d35bc054db72b7d8c3237c332
Abelardo Marquez <ajmarquezg@gmail.com>
2020-07-12 01:47:35 +0200

    First Commit
    - Add Mongoose and Validator
    - Set model for User and Task
    - Add basic validation for fields

---
+++ src/db/mongoose.js
@@ -0,0 +1,77 @@
+const mongoose = require('mongoose')
+const validator = require('validator')
+
+mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true
+})
+
+//User Model
+const User = mongoose.model('User', {
+    name: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    age: {
+        type: Number,
+        default: 0,
+        validate(value) {
+            if (value < 0) {
+                throw new Error('Age must be a positive number')
+            }
+        }
+        
+    },
+    email: {
+        type: String,
+        required: true,
+        trim: true,
+        lowercase: true,
+        validate(value) {
+            if (!validator.isEmail(value)) {
+                throw new Error('Email is invalid')
+            }
+        }
+    },
+    password: {
+        type: String,
+        required: true,
+        trim: true,
+        minlength: 7,
+        validate(value) {
+            if (value.toLowerCase().includes('password')) {
+                throw new Error('Passwords must be longer than 6 character and must not include the word \'password\'')
+            }
+        }
+    }
+})
+
+//Task Model
+const Task = mongoose.model('Task', {
+    description: {
+        type: String,
+        trim: true,
+        required: true
+    },
+    completed: {
+        type: Boolean,
+        default: false
+    }
+})
+
+
+// const newUser = new User({
+//     name: 'Morrys           ',
+//     email: 'FRANK@AOL.com',
+//     password: '4word'
+
+// })
+
+// newUser.save().then(() => {
+//     console.log(newUser)
+// }).catch((error) => {
+//     console.log('Error!!!! ->', error )
+// })
+
